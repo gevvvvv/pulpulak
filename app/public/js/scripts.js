894f132b8dab34f940ab1f777598d3ff0259b3bb
Vartan <vartan.bg@gmail.com>
2015-10-24 22:13:41 +0400

    testing if geolocation is not awailable

--- app/public/js/scripts.js
+++ app/public/js/scripts.js
@@ -69,7 +69,6 @@ $(document).ready(function(){
 			alert("Your browser does not support geolocation");
 		},
 		always: function() {
-			console.log(position, 'position');
 		}
 	});
 	pulpulakDestination = function (originLat, originLon, lat, lon){
